e29441d5701d776d3f765befac256b406de3e391
RedCr4zy <aleskiki2009@gmail.com>
2026-04-04 14:15:21 +0200

    Fixed bugs

--- gameManager.js
+++ gameManager.js
@@ -45,6 +45,8 @@ function generateRoom() {
 
 function applyMalusToActivePlayer(room, currentPlayerToken) {
     const roomCode = room;
+    console.log(roomCode);
+    console.log(rooms[roomCode]);
     console.log(rooms[roomCode].gameState.playerOrder.length);
     if (rooms[roomCode].gameState.playerOrder.length <= 2) {
         console.log("Pas de malus à 2 joueurs")
